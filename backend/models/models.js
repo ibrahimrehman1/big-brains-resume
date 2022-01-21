20ae6acf5bbf723d22d5aec23e680b6e7687e79f
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2022-01-21 20:58:39 +0500

    Created separate schemas for forms and templates

--- backend/models/models.js
+++ backend/models/models.js
@@ -1,8 +1,8 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 
-const resumeSchema = Schema({
+const resumeFormSchema = Schema({
   fullName: {
     type: String,
     trim: true,
@@ -11,12 +11,11 @@ const resumeSchema = Schema({
   designation: {
     type: String,
     trim: true,
-
   },
   summary: {
     type: String,
     trim: true,
-    minLength: [50, 'Summary is too Short!']
+    minLength: [50, "Summary is too Short!"],
   },
   skills: {
     type: Map,
@@ -48,7 +47,52 @@ const resumeSchema = Schema({
   },
 });
 
-const cvSchema = Schema({
+const resumeTemplateSchema = Schema({
+  fullName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  designation: {
+    type: String,
+    trim: true,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    minLength: [50, "Summary is too Short!"],
+  },
+  skills: {
+    type: Map,
+    default: {},
+  },
+  education: {
+    type: Array,
+    default: [],
+  },
+  projects: {
+    type: Array,
+    default: [],
+  },
+  contactDetails: {
+    type: Map,
+    default: {},
+  },
+  languages: {
+    type: Array,
+    default: [],
+  },
+  interests: {
+    type: Array,
+    default: [],
+  },
+  certifications: {
+    type: Array,
+    default: [],
+  },
+});
+
+const cvFormSchema = Schema({
   fullName: {
     type: String,
     trim: true,
@@ -61,7 +105,56 @@ const cvSchema = Schema({
   aboutMe: {
     type: String,
     trim: true,
-    minLength: [50, 'About Me is too Short!']
+    minLength: [50, "About Me is too Short!"],
+  },
+  skills: {
+    type: Map,
+    default: {},
+  },
+  detailedEducation: {
+    type: Array,
+    default: [],
+  },
+  projects: {
+    type: Array,
+    default: [],
+  },
+  contactDetails: {
+    type: Map,
+    default: {},
+  },
+  languages: {
+    type: Array,
+    default: [],
+  },
+  interests: {
+    type: Array,
+    default: [],
+  },
+  certifications: {
+    type: Array,
+    default: [],
+  },
+  workExperience: {
+    type: Array,
+    default: [],
+  },
+});
+
+const cvTemplateSchema = Schema({
+  fullName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  designation: {
+    type: String,
+    trim: true,
+  },
+  aboutMe: {
+    type: String,
+    trim: true,
+    minLength: [50, "About Me is too Short!"],
   },
   skills: {
     type: Map,
@@ -98,20 +191,20 @@ const cvSchema = Schema({
 });
 
 const feedbackSchema = Schema({
-    emojis: {
-        type: String,
-        required: true
-    },
-    comments : {
-        type: String,
-        required: true,
-        minLength: [50, 'Feedback is too Short!']
-    },
-    type: {
-        type: String,
-        required: true
-    }
-})
+  emojis: {
+    type: String,
+    required: true,
+  },
+  comments: {
+    type: String,
+    required: true,
+    minLength: [50, "Feedback is too Short!"],
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+});
 
 const userSchema = Schema({
   firstName: {
@@ -129,42 +222,43 @@ const userSchema = Schema({
     required: true,
     trim: true,
     unique: true,
-    minLength: [5, 'Username too Short!']
+    minLength: [5, "Username too Short!"],
   },
   emailAddress: {
     type: String,
     required: true,
     trim: true,
     unique: true,
-    validate: [validator.isEmail, "Email format not correct!"]
+    validate: [validator.isEmail, "Email format not correct!"],
   },
   password: {
     type: String,
     required: true,
     trim: true,
-    validate: [validator.isStrongPassword, "Password not strong Enough!"] 
+    validate: [validator.isStrongPassword, "Password not strong Enough!"],
     // { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1}
   },
-  userCV: {
-    type: [cvSchema],
-    default: [],
+  userFormCV: {
+    type: [{ type:  Types.ObjectId, ref: "CVForm"}],
   },
-  userResume: {
-    type: [resumeSchema],
-    default: [],
+  userFormResume: {
+    type: [{type:  Types.ObjectId, ref: "ResumeForm"}],
+  },
+  userTemplateCV: {
+    type: [{ type:  Types.ObjectId, ref: "CVTemplate"}],
+  },
+  userTemplateResume: {
+    type: [{type:  Types.ObjectId, ref: "ResumeTemplate"}],
   },
   userFeedback: {
-    type: [feedbackSchema],
-    default: [],
+    type: [{type: Types.ObjectId, ref: "Feedback"}],
   },
 });
 
-userSchema.pre("save", async function (next){
+userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt();
   this.password = await bcrypt.hash(this.password, salt);
   next();
-})
+});
 
-
-
-module.exports = model("user", userSchema)
+module.exports = model("user", userSchema);
