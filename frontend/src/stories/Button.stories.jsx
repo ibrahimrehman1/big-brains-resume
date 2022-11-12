a72ae6adcb6b3dcfafc7176142b6952515debda1
ibrahimrehman1 <ibrahimrehman0346@gmail.com>
2022-11-12 12:24:15 +0500

    add Button stories

--- frontend/src/stories/Button.stories.jsx
+++ frontend/src/stories/Button.stories.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import Button from "../components/Button";
 
 export default {
-    title: "Button",
-    component: Button,
-    argTypes: {clickHandler: {action: "Navigate"}}
-}
+  title: "Components/Button",
+  component: Button,
+  argTypes: { clickHandler: { action: "Navigate" } },
+};
 
-const Template = (props) => <Button {...props} />
+const Template = (args) => <Button {...args} />;
 
+export const Red = Template.bind({});
+Red.args = {
+  styles: {
+    backgroundColor: "red",
+    fontWeight: "bold",
+  },
+  variant: "contained",
+  size: "sm",
+  text: "Hello World",
+};
 
-export const Red = Template.bind({})
-Red.props = {
-    styles: {
-        backgroundColor: "red"
-    },
-    variant: "contained",
-    size: "sm",
-    text: "Hello World"
-}
+export const Green = Template.bind({});
+Green.args = {
+  styles: {
+    backgroundColor: "green",
+    fontWeight: "bold",
+  },
+  variant: "contained",
+  size: "md",
+  text: "Hello World",
+};
